87361f02da8fc5367adf6bd0e69fbb6707025141
AllmightFRC <niordpereira@gmail.com>
2025-02-20 19:25:39 -0300

    Criando página de endereço

---
+++ js/endereco.js
@@ -0,0 +1,12 @@
+const INPUT_BAIRRO = document.getElementById('bairro');
+const INPUT_CIDADE = document.getElementById('cidade');
+const INPUT_UF = document.getElementById('uf');
+
+INPUT_CEP.addEventListener('blur', () => {
+        let endereco = {
+            logadouro: 'Rua Barca Velha',
+            bairro: 'Quintino Cunha'
+            cidade: 'Fortaleza',
+            uf: 'CE'
+        }
+})
