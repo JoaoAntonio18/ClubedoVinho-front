b8a4142b1ac94d12f5cf116d4eef3c05cc6d33a8
AllmightFRC <niordpereira@gmail.com>
2025-03-20 19:56:26 -0300

    Lendo dados da api e atualizando a página de produtos

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ClubedoVinho-front
 Aplicação em HTML, CSS,  bootstrap para o frontend de um ecommerce de vinhos
+
+
+## Como rodar a aplicação
+    1 - Abrir com o navegador o index.html do clubedovinho-front 
+    2 - Subir o servidor que simula o banco de dados
+    3 - Entrar com o terminal na pasta do projeto e executar
+    ``` shell
+    npx json-server db.json
+    ```
--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,77 @@
 const TABLE = document.getElementById('table-products');
 
+
+fetch('http://localhost:3000/products')
+    .then(res => res.json())
+    .then(dados => {
+        dados.forEach((product) => {
+            TABLE.innerHTML += `
+                <tr>
+                    <td>${product.id}</td>
+                    <td>${product.name}</td>
+                    <td>${product.category}</td>
+                    <td> <img onclick="abrirModal('${product.name}', '${product.image}')" data-bs-toggle="modal" data-bs-target="#exampleModal" src="${product.image}" width="100px"> </td>
+                    <td>${product.quantity}</td>
+                    <td>${product.price}</td>
+                    <td>
+                        <a href="#" class="btn btn-outline-warning btn-sm">
+                            Editar
+                        </a>
+                        <a href="#" onclick="excluir(${product.id})" class="btn btn-outline-danger btn-sm">
+                            Excluir
+                        </a>
+                    </td>
+                </tr>
+            `;
+        });
+    });
+
+    function excluir(id) {
+        if (false === confirm('Confirma ou sem firma?')) {
+            return;
+        }
+    
+        fetch(`http://localhost:3000/products/${id}`, {
+            method: 'DELETE'
+        });
+    
+        // location.href = "";
+    }
+
+
+    function addProduct() {
+        event.preventDefault();
+    
+        let dados = {
+            name: document.getElementById('name').value,
+            price: document.getElementById('price').value,
+            category: document.getElementById('category').value,
+            quantity: document.getElementById('quantity').value,
+            image: document.getElementById('image').value,
+        };
+    
+        fetch('http://localhost:3000/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dados)
+        });
+    
+        alert('Pronto, cadastrado com sucesso');
+        location.href = "";
+    }
+    
+    function abrirModal(nome, imagem) {
+        document.getElementById('modal_produto_nome').innerHTML = nome;
+        document.getElementById('modal_produto_body').innerHTML = `
+            <img src="${imagem}" width="100%">
+        `;
+    } 
+
+
+    
+
 let dados = [
     {
         id: 1,
@@ -57,4 +129,27 @@ dados.forEach(product => {
 function openModal(imagem, nome) {
     document.getElementById("modalImage").src = imagem;
     document.getElementById("imageModalLabel").innerText = nome; 
+}
+
+function addProduct() {
+    event.preventDefault();
+
+    let dados = {
+        name: document.getElementById('nome').value,
+        price: document.getElementById('price').value,
+        category: document.getElementById('categoria').value,
+        quantity: document.getElementById('quantity').value,
+        image: document.getElementById('image').value,
+    }
+
+    fetch('http://localhost:3000/products', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados)
+    });
+
+    alert('Produto cadastrado com sucesso!');
+    location.href = "";
 }
